88020880350a7711dd36e9b6fa0ac9fe8d841454
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-05-31 12:30:51 +0530

    changed message

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,5 +1,6 @@
 public class EmpWageComputation {
 	public static void main(String args[]){
+		System.out.println("Welcome to Employee Wage Computation Program");
 		//constants
 		int IS_FULL_TIME=1;
 		//Computation
