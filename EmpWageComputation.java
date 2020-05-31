1ce84becbe01879ae86136c28cd69cafc02ac50c
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-05-31 12:16:02 +0530

    check employee presence uc1

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,13 @@
-class Welcome{
+public class EmpWageComputation {
 	public static void main(String args[]){
-		System.out.println("Welcome to Employee Wage Comoutation Program on Master Branch");
-
+		//constants
+		int IS_FULL_TIME=1;
+		//Computation
+		double empCheck=Math.floor(Math.random() * 10) %2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
 
