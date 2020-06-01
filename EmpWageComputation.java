2911244b8c35c0ff77eded91c707c48aa7c3ae9b
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-01 08:30:04 +0530

    refactor the previous code using method and object

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,15 +1,20 @@
 public class EmpWageComputation {
-	public static final int IS_PART_TIME=1;
-	public static final int IS_FULL_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
+	public final int IS_PART_TIME=1;
+	public final int IS_FULL_TIME=2;
+	public final int EMP_RATE_PER_HOUR=20;
+	public final int NUM_OF_WORKING_DAYS=20;
+	public final int MAX_HRS_IN_MONTH=100;
 
 	public static void main(String args[]){
 		System.out.println("Welcome to Employee Wage Computation Program");
+		EmpWageComputation emp=new EmpWageComputation();
+		emp.getEmpWage();
+	}
 
+	public void getEmpWage(){
 		//variables
 		int empHrs=0;
+		int empWage=0;
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
 		//Computation
@@ -27,6 +32,7 @@ public class EmpWageComputation {
 					empHrs=0;
 			}
 				totalEmpHrs +=empHrs;
+				empWage=empHrs * EMP_RATE_PER_HOUR;
 				System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		}
 				int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
@@ -34,3 +40,4 @@ public class EmpWageComputation {
 	}
 
 }
+
