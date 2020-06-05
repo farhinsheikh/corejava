fddb569333fbee8da6e2d370865e28f4f9543300
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-06 00:12:46 +0530

    refactor to use array list instead of array

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,34 +1,67 @@
-import java.util.Scanner;
+import java.util.*;
 
-class Employee implements computerEmpWage{
-
-	int workingDays = 0;
-	int totalHours = 0;
-	int empHours = 0;
-	int empRatePerHour = 20;
-	int salary = 0;
-	public void companyMethod(String companyName, int wage, int days, int hours){
-	int totalWage = ( wage * days * hours);
-	System.out.println("Total wage for a employee working full-time for a month:"+totalWage);
-}
-
-
-public static void main(String[] args) {
-	Employee ep = new Employee();
+class Employee {
+	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
+	ArrayList<CompanyEmpWage> cp = new ArrayList<CompanyEmpWage>();
+	boolean end_key = true;
+	System.out.println(" Welcome to Employee Wage Computation Program\n");
+	while (end_key) {
+	System.out.println("	1: Add	2: View your company's total wage	9: Exit");
+	int choice = sc.nextInt();
+	int i = 0;
+	switch (choice) {
+		case 1:
+			sc.nextLine();
+			System.out.println(" Enter name of your company ");
+			String name = sc.nextLine();
+			System.out.println(" Enter the wage per hour of your company :");
+			int wage = sc.nextInt();
+			System.out.println(" Enter the number of working day in a month :");
+			int days = sc.nextInt();
+			System.out.println(" Enter the number of working hours in a day :");
+			int hours = sc.nextInt();
+			cp.add(new CompanyEmpWage(name ,wage, days, hours));
+		break;
+		case 2:
+			sc.nextLine();
+			System.out.println(" Enter the name of your company to view total wage:");
+			String update = sc.nextLine();
+			Iterator<CompanyEmpWage> iter = cp.iterator();
+		while (iter.hasNext()) {
+			CompanyEmpWage data = iter.next();
+			if (update.equals(data.getRecord())) {
+				System.out.println("	Your company's total wage is:"+data.getValues());
+		}
+	}
 
-	System.out.println("Welcome to Employee Wage Computation Program\n");
-	String companyName = "Tata";
-	System.out.println("Company name :"+companyName);
-	System.out.println("Enter the wage per hour of your company :");
-	int wage = sc.nextInt();
-	System.out.println("Enter the number of working day in a month :");
-	int days = sc.nextInt();
-	System.out.println("Enter the number of working hours in a day :");
-	int hours = sc.nextInt();
-	ep.companyMethod(companyName, wage, days, hours);
+		break;
+		case 9:
+			end_key = false; break;
+		default:
+			System.out.println("	Enter valid input");
+		break;
+		}
 	}
 }
-	interface computerEmpWage{
-	public void companyMethod(String companyName,int wage,int days,int hours);
+}
+class CompanyEmpWage{
+	String name;
+	int wage;
+	int days;
+	int hours;
+	int totalWage;
+	CompanyEmpWage(String name,int wage, int days, int hours){
+	this.name = name;
+	this.wage = wage;
+	this.days = days;
+	this.hours = hours;
+	this.totalWage = (wage * days * hours);
+}
+public int getValues(){
+	return this.totalWage;
+}
+public String getRecord(){
+	return this.name;
+	}
 }
